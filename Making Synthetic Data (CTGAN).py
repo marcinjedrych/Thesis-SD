43b9a353c49189d39d25b47592fad8315e3c166d
marcinjedrych <marcin.jedrych7@outlook.com>
2025-04-09 15:09:40 +0200

    Update Making Synthetic Data (CTGAN).py

--- Making Synthetic Data (CTGAN).py
+++ Making Synthetic Data (CTGAN).py
@@ -33,25 +33,25 @@ import os
 # 1. Process 'no_missing.xlsx' and save synthetic data
 df = pd.read_excel(r"Data/Original/no_missing.xlsx")
 synthetic_data = generate_synthetic_data(df)
-synthetic_data.to_excel(r"Data/Synthetic/synthetic_no_missing.xlsx", index=True)
+synthetic_data.to_excel(r"Data/Synthetic/synthetic_no_missing.xlsx", index=False)
 
 # 2. Complete Case Analysis (CCA)
 for filename in os.listdir(r"Data/Original/Complete Case Analysis"):
     if filename.endswith(".xlsx"):
         df = pd.read_excel(rf"Data/Original/Complete Case Analysis/{filename}")
         synthetic_data = generate_synthetic_data(df)
-        synthetic_data.to_excel(rf"Data/Synthetic/Complete Case Analysis/{filename}", index=True)
+        synthetic_data.to_excel(rf"Data/Synthetic/Complete Case Analysis/{filename}", index=False)
 
 # 3. Multiple Imputation
 for filename in os.listdir(r"Data/Original/Multiple Imputation"):
     if filename.endswith(".xlsx"):
         df = pd.read_excel(rf"Data/Original/Multiple Imputation/{filename}")
         synthetic_data = generate_synthetic_data(df)
-        synthetic_data.to_excel(rf"Data/Synthetic/Multiple imputation/{filename}", index=True)
+        synthetic_data.to_excel(rf"Data/Synthetic/Multiple imputation/{filename}", index=False)
 
 # 4. Learned NA
 for filename in os.listdir(r"Data/Original/Learned NA"):
     if filename.endswith(".xlsx"):
         df = pd.read_excel(rf"Data/Original/Learned NA/{filename}")
         synthetic_data = generate_synthetic_data(df)
-        synthetic_data.to_excel(rf"Data/Synthetic/Learned NA/{filename}", index=True)
+        synthetic_data.to_excel(rf"Data/Synthetic/Learned NA/{filename}", index=False)
